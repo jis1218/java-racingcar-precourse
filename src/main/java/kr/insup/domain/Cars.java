890b11d362b7f8ccd59672962476adca5d5ccb08
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-05-01 00:20:28 +0900

    feat(cars): 차를 이동시키는 메서드 추가
     - CarsTest에 차가 잘 움직이는지 확인하는 테스트 추가
     - Cars에 차를 움직이게 하는 메서드 추가
     - Cars, CarsTest 리팩토링

--- src/main/java/kr/insup/domain/Cars.java
+++ src/main/java/kr/insup/domain/Cars.java
@@ -5,17 +5,37 @@ import java.util.List;
 
 public class Cars {
 
-    List<Car> cars = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
+    private int size;
 
     public Cars(String input) {
         String[] carNames = input.split(",");
-        for(String carName : carNames){
+        for (String carName : carNames) {
             carName = carName.trim();
             cars.add(new Car(carName));
         }
+        size = cars.size();
     }
 
-    public int size(){
-        return cars.size();
+    public int size() {
+        return this.size;
+    }
+
+    public void moveCars(int[] oneSet) {
+        for (int i = 0; i < this.size; i++) {
+            getCar(i).moveCar(oneSet[i]);
+        }
+    }
+
+    public Car getCar(int i) {
+        return cars.get(i);
+    }
+
+    public int[] getCarsPosition() {
+        int[] positions = new int[this.size];
+        for (int i = 0; i < this.size; i++) {
+            positions[i] = getCar(i).checkPosition();
+        }
+        return positions;
     }
 }
--- src/test/java/kr/insup/domain/CarsTest.java
+++ src/test/java/kr/insup/domain/CarsTest.java
@@ -1,5 +1,6 @@
 package kr.insup.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,14 +8,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
 
+    private static final int TRIAL = 3;
+    Cars cars;
+
+    @DisplayName("차 생성")
+    @BeforeEach
+    void makeCar() {
+        cars = new Cars("a, b, c, d");
+    }
+
     @DisplayName("차 컬렉션 생성")
     @Test
     void 입력값_받아_차_컬렉션_생성() {
-        //given
-        //when
-        Cars cars = new Cars("a, b, c, d");
-
         //then
         assertThat(cars.size()).isEqualTo(4);
+    }
+
+    @DisplayName("차가 잘 움직이는지 확인")
+    @Test
+    void 차가_잘_움직이는지_확인() {
+        //given
+        int[][] numbers =
+                {
+                        {3, 4, 7, 1},
+                        {4, 2, 6, 3},
+                        {2, 5, 8, 2}
+                };
+        int[] answer = {1, 2, 3, 0};
+
+        //when
+        for (int i = 0; i < TRIAL; i++) {
+            cars.moveCars(numbers[i]);
+        }
+        int[] positions = cars.getCarsPosition();
+
+        //then
+        assertThat(positions).containsExactly(answer);
     }
 }
