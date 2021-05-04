e0b4b10580ac40bab0603c527770835ff9b06ade
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-05-04 22:14:53 +0900

    refactor: RandomValueGenerator 리팩터링
    변수 선언 순서 변경

--- src/main/java/kr/insup/utils/RandomValueGenerator.java
+++ src/main/java/kr/insup/utils/RandomValueGenerator.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 public class RandomValueGenerator {
 
-    private static Random random = new Random();
-
     /**
      * 0~9 사이의 값을 위한 bound
      */
     private static int MAX_BOUND = 10;
+
+    private static Random random = new Random();
 
     public static int generateRandomValue() {
         return random.nextInt(MAX_BOUND);
